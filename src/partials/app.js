13ed8b15976b49126c406e83b90566a33145cd7d
MaxusGuitar <maxim1999chikhanov@gmail.com>
2021-11-21 18:38:45 +0200

    THE END!!!!

--- src/partials/app.js
+++ src/partials/app.js
@@ -1,30 +1,57 @@
 import cardInf from "../templates/card.hbs"
+import flagMini from "../templates/flag.hbs"
 import API from "./fetchCountries.js"
 import debounce from "lodash.debounce"
-import Notiflix from "notiflix"
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const cardCountry = document.querySelector('.container')
 const runSearch = document.querySelector('.run-search > input')
 
-runSearch.addEventListener('input', debounce(() => {
-    //e.preventDefault() // для аниперезагрузки стр при каждом поиске
+// runSearch.addEventListener('input', debounce(() => {
+//     e.preventDefault() // для аниперезагрузки стр при каждом поиске
 
-    //const form = e.currentTarget
-    const queryCountry = runSearch.value //ссылка, что бы забрать текст из инпута
+//     const form = e.currentTarget
+//     const queryCountry = runSearch.value //ссылка, что бы забрать текст из инпута
 
-    API.fetchCountry(queryCountry)
-        .then(countrySearch)
-        .then(() => console.clear()) // очищает консоль после каждого поиска
-        .catch(showError)
-}, 300))
+//     API.fetchCountry(queryCountry)
+//         .then(countrySearch)
+//         .then(() => console.clear()) // очищает консоль после каждого поиска
+//         .catch(showError)
+// }, 300))
 
-function showError() {
-    Notiflix.Notify.failure('Oops, there is no country with that name')
-        }
+// function showError() {
+//     Notiflix.Notify.failure('Oops, there is no country with that name')
+//         }
         
-function countrySearch(country) {
-    const card = cardInf(country[0])
-    cardCountry.innerHTML = card
-    console.log(country);
+// function countrySearch(country) {
+//     const card = cardInf(country[0])
+//     cardCountry.innerHTML = card
+//     console.log(country);
     
-    }
+//     }
+runSearch.addEventListener('input', debounce(LookingForCountry, 300));
+
+function LookingForCountry(a) {
+    
+    const searchCountry = runSearch.value.trim();
+    if (runSearch.value === '') {
+        cardCountry.textContent = '';
+        a.data === ' '
+        return;
+    } 
+    
+    {
+        API.fetchCountry(`${searchCountry}`)
+            .then(country => {
+        if (country.length > 10 ) {
+            Notify.info("Too many matches found. Please enter a more specific name.");
+        } else if (country.length > 2 & country.length < 10) {
+            const content = flagMini(country);
+            cardCountry.innerHTML = content;
+        } else {
+            const markup = cardInf(country);
+            cardCountry.innerHTML = markup;
+     }
+            }).catch(() => {Notify.failure("Oops, there is no country with that name")})
+}
+}
--- src/partials/fetchCountries.js
+++ src/partials/fetchCountries.js
@@ -2,13 +2,16 @@ const BASE_URL = 'https://restcountries.com/v2'
 
 function fetchCountry(name) {
     return fetch(`${BASE_URL}/name/${name}`)
-        .then(response => {
+        .then(function(response) {
             if (!response.ok) {
-        throw new Error(response.statusText);
-    }
+                throw new Error(response.statusText);
+            }
             
-            return response.json();
+            return response
             
+        })
+        .then(response => {
+            return response.json()
         })
 }
 
